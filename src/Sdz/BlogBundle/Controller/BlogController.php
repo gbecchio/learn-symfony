0171b8aa39598f8d532837218cf52bbec8c690ad
Gregory Becchio <gbecchio@yahoo.fr>
2013-11-29 18:26:41 +0100

    One to many and One to One

--- src/Sdz/BlogBundle/Controller/BlogController.php
+++ src/Sdz/BlogBundle/Controller/BlogController.php
@@ -4,11 +4,23 @@ namespace Sdz\BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+// Attention à bien ajouter ce use en début de contrôleur
+use Sdz\BlogBundle\Entity\Article;
+use Sdz\BlogBundle\Entity\Image;
+use Sdz\BlogBundle\Entity\Commentaire;
 
 class BlogController extends Controller
 {
     public function indexAction()
     {
+      $doctrine = $this->getDoctrine();
+      $doctrine2 = $this->get('doctrine');
+      //var_dump($doctrine = $this->getDoctrine());
+      $em = $doctrine->getManager();
+      $repository_article = $em->getRepository('SdzBlogBundle:Article');
+      $repository_article2 = $em->getRepository('Sdz\BlogBundle\Entity\Article');
+      
+      //var_dump($repository_article);
       // …
       $mailer = $this->container->get('mailer'); // On a donc accès au conteneur
       $mailer2 = $this->get('mailer'); // On a donc accès au conteneur
@@ -67,21 +79,28 @@ txt;
     }
     public function voirAction($id)
     {
-        // …
-        $article = array
-        (
-            'id'      => 1,
-            'titre'   => 'Mon weekend a Phi Phi Island !',
-            'auteur'  => 'winzou',
-            'contenu' => 'Ce weekend était trop bien. Blabla…',
-            'date'    => new \Datetime()
-        );
-        // Puis modifiez la ligne du render comme ceci, pour prendre en compte l'article :
-        return $this->render('SdzBlogBundle:Blog:voir.html.twig', array
-            (
-                'article' => $article
-            )
-        );
+      // On récupère l'EntityManager
+      $em = $this->getDoctrine()
+                 ->getManager();
+
+      // On récupère l'entité correspondant à l'id $id
+      $article = $em->getRepository('SdzBlogBundle:Article')
+                    ->find($id);
+
+      if($article === null)
+      {
+        throw $this->createNotFoundException('Article[id='.$id.'] inexistant.');
+      }
+
+      // On récupère la liste des commentaires
+      $liste_commentaires = $em->getRepository('SdzBlogBundle:Commentaire')
+                               ->findAll();
+
+      // Puis modifiez la ligne du render comme ceci, pour prendre en compte l'article :
+      return $this->render('SdzBlogBundle:Blog:voir.html.twig', array(
+        'article'      => $article,
+        'liste_commentaires' => $liste_commentaires
+      ));
     }
     public function voirSlugAction($slug, $annee, $format)
     {
@@ -91,8 +110,47 @@ txt;
     }
     public function ajouterAction()
     {
-        // Ici le contenu de la méthode
-        return $this->render('SdzBlogBundle:Blog:ajouter.html.twig');
+      // Création de l'entité Article
+      $article = new Article();
+      $article->setTitre('Mon dernier weekend');
+      $article->setContenu("C'était vraiment super et on s'est bien amusé.");
+      $article->setAuteur('winzou');
+
+      // Création d'un premier commentaire
+      $commentaire1 = new Commentaire();
+      $commentaire1->setAuteur('winzou');
+      $commentaire1->setContenu('On veut les photos !');
+
+      // Création d'un deuxième commentaire, par exemple
+      $commentaire2 = new Commentaire();
+      $commentaire2->setAuteur('Choupy');
+      $commentaire2->setContenu('Les photos arrivent !');
+
+      // On lie les commentaires à l'article
+      $commentaire1->setArticle($article);
+      $commentaire2->setArticle($article);
+
+      // On récupère l'EntityManager
+      $em = $this->getDoctrine()->getManager();
+
+      // Étape 1 : On persiste les entités
+      $em->persist($article);
+      // Pour cette relation pas de cascade, car elle est définie dans l'entité Commentaire et non Article
+      // On doit donc tout persister à la main ici
+      $em->persist($commentaire1);
+      $em->persist($commentaire2);
+
+      // Étape 2 : On déclenche l'enregistrement
+      $em->flush();
+      
+      // Reste de la méthode qu'on avait déjà écrit
+      if ($this->getRequest()->getMethod() == 'POST') {
+        $this->get('session')->getFlashBag()->add('info', 'Article bien enregistré');
+        return $this->redirect( $this->generateUrl('sdzblog_voir', array('id' => $article->getId())) );
+      }
+
+
+      return $this->render('SdzBlogBundle:Blog:ajouter.html.twig');
     }
     public function menuAction($nombre) // Ici, nouvel argument $nombre, on l'a transmis via le render() depuis la vue
     {
@@ -115,21 +173,55 @@ txt;
     
   public function modifierAction($id)
   {
-    // Ici, on récupérera l'article correspondant à $id
+     // On récupère l'EntityManager
+    $em = $this->getDoctrine()
+               ->getManager();
 
-    // Ici, on s'occupera de la création et de la gestion du formulaire
+    // On récupère l'entité correspondant à l'id $id
+    $article = $em->getRepository('SdzBlogBundle:Article')
+                  ->find($id);
 
-    $article = array(
-      'id'      => 1,
-      'titre'   => 'Mon weekend a Phi Phi Island !',
-      'auteur'  => 'winzou',
-      'contenu' => 'Ce weekend était trop bien. Blabla…',
-      'date'    => new \Datetime()
-    );
-        
-    // Puis modifiez la ligne du render comme ceci, pour prendre en compte l'article :
-    return $this->render('SdzBlogBundle:Blog:modifier.html.twig', array(
-      'article' => $article
+    if ($article === null) {
+      throw $this->createNotFoundException('Article[id='.$id.'] inexistant.');
+    }
+
+    // On récupère toutes les catégories :
+    $liste_categories = $em->getRepository('SdzBlogBundle:Categorie')
+                           ->findAll();
+
+    // On boucle sur les catégories pour les lier à l'article
+    foreach($liste_categories as $categorie)
+    {
+      $article->addCategorie($categorie);
+    }
+
+    // Inutile de persister l'article, on l'a récupéré avec Doctrine
+
+    // Étape 2 : On déclenche l'enregistrement
+    $em->flush();
+
+    return new Response('OK');
+  }
+  public function modifierImageAction($id_article)
+  {
+    $em = $this->getDoctrine()->getManager();
+
+    // On récupère l'article
+    $article = $em->getRepository('SdzBlogBundle:Article')->find($id_article);
+    $article->getImage();
+    // On modifie l'URL de l'image par exemple
+    $article->getImage()->setUrl('https://lh4.googleusercontent.com/-TtZ8a2i9KsM/UYFKqR5KewI/AAAAAAAABiI/VvuOY_VEJQc/w1600-h900/default_cover_2_ae125d34a6150400a2a97f22e218a904.jpg');
+    $image = $article->getImage();
+    // On n'a pas besoin de persister notre article (si vous le faites, aucune erreur n'est déclenchée, Doctrine l'ignore)
+    // Rappelez-vous, il l'est automatiquement car on l'a récupéré depuis Doctrine
+
+    // Pas non plus besoin de persister l'image ici, car elle est également récupérée par Doctrine
+
+    // On déclenche la modification
+    $em->flush();
+
+    return $this->render('SdzBlogBundle:Blog:modifierImage.html.twig', array(
+      'image' => $image, 'id_article' => $id_article, 'article' => $article
     ));
   }
 }
